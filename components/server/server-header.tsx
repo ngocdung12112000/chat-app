2d6621eede59945b50818a67d3cb67ea951420e4
Ngoc Duxng <dungbachngoc20@gmail.com>
2023-12-31 16:01:35 +0700

    add server settings

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -4,6 +4,7 @@ import { ServerWithMembersAndProfiles } from "@/types"
 import { MemberRole } from "@prisma/client"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "../ui/dropdown-menu"
 import { ChevronDown, LogOut, PlusCircle, Settings, Trash, UserPlus, Users } from "lucide-react"
+import { useModal } from "@/hooks/use-modal-store"
 
 interface ServerHeaderProps {
     server: ServerWithMembersAndProfiles
@@ -13,6 +14,7 @@ interface ServerHeaderProps {
 export const ServerHeader = ({
     server, role
 }: ServerHeaderProps) => {
+    const { onOpen } = useModal()
     const isAdmin = role === MemberRole.ADMIN
     const isModerator = isAdmin || role === MemberRole.MODERATOR
 
@@ -38,6 +40,7 @@ export const ServerHeader = ({
             >
                 {isModerator && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("invite", {server})}
                         className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
                     >
                         Invite People
@@ -46,6 +49,7 @@ export const ServerHeader = ({
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("editServer", {server})}
                         className="px-3 py-2 text-sm cursor-pointer"
                     >
                         Server Settings
@@ -54,6 +58,7 @@ export const ServerHeader = ({
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("members", {server})}
                         className="px-3 py-2 text-sm cursor-pointer"
                     >
                         Manage Members
---
+++ components/user-avatar.tsx
@@ -0,0 +1,6 @@
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+
+interface UserAvatarProps {
+    src?: string
+    className?: string
+}
