50a99834a4c4e2a73b0b98c36654384646558a57
Ngoc Duxng <dungbachngoc20@gmail.com>
2024-01-01 22:17:35 +0700

    add delete and leave server

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -67,6 +67,7 @@ export const ServerHeader = ({
                 )}
                 {isModerator && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("createChannel", {server})}
                         className="px-3 py-2 text-sm cursor-pointer"
                     >
                         Create Channel
@@ -78,6 +79,7 @@ export const ServerHeader = ({
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
+                    onClick={() => onOpen("deleteServer", {server})}
                         className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
                     >
                         Delete Server
@@ -86,6 +88,7 @@ export const ServerHeader = ({
                 )}
                 {!isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("leaveServer", {server})}
                         className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
                     >
                         Leave Server
